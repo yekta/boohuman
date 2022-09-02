15c9dbdc679d028aede56f8f3e59e88d3503c54f
yekta <ngyekta@gmail.com>
2022-09-02 16:32:11 +0300

    CollectionEntryCard srcset

--- src/lib/components/CollectionEntryCard.svelte
+++ src/lib/components/CollectionEntryCard.svelte
@@ -9,16 +9,42 @@
 	function setActiveEntry(entry: TDBCollectionEntry) {
 		activeEntry.set(entry);
 	}
+
+	const sizes =
+		'(min-width: 1280px) 25vw, (min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw';
+
+	const imageSizes = [512, 768, 1024, 1536, 2048];
+	function srcSetFromUrl(url: string) {
+		const { pathname, hostname, protocol } = new URL(url);
+		const prefix = '/boohuman/';
+		const rest = pathname.slice(prefix.length);
+		let srcset = '';
+		imageSizes.forEach((size) => {
+			srcset += `${protocol}//${hostname}${prefix}tr:w-${size}/${rest} ${size}w, `;
+		});
+		return srcset;
+	}
+
+	function loadImage(url: string) {
+		const img = new Image();
+		img.src = url;
+	}
 </script>
 
-<button on:click={() => setActiveEntry(entry)} class="w-full overflow-hidden group">
+<button
+	on:mouseenter={() => loadImage(entry.imageUrl)}
+	on:click={() => setActiveEntry(entry)}
+	class="w-full overflow-hidden group"
+>
 	<img
 		in:receive|local={{ key: entry.id }}
 		out:send|local={{ key: entry.id }}
 		src={entry.imageUrl}
+		srcset={srcSetFromUrl(entry.imageUrl)}
 		alt={entry.name}
 		width={entry.imageWidth}
 		height={entry.imageHeight}
+		{sizes}
 		class="select-none transition duration-300 origin-bottom group-hover:scale-102"
 	/>
 </button>
