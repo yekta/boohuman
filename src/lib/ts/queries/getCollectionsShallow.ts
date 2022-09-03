8da0bb53689fd4a9d98f923bff006c952b19c81f
yekta <40706601+yekta@users.noreply.github.com>
2022-09-03 07:36:23 +0300

    Get collections query

---
+++ src/lib/ts/queries/getCollectionsShallow.ts
@@ -0,0 +1,28 @@
+import { supabase } from '$ts/constants/supabase';
+import type { TDBCollectionShallow } from '$ts/types/db';
+import type { PostgrestError } from '@supabase/supabase-js';
+
+export async function getCollectionsShallow() {
+	const { data, error }: { data: TDBCollectionShallow[] | null; error: PostgrestError | null } =
+		await supabase.from('art_collection').select(
+			`
+        id,
+        name,
+        slug,
+        imageUrl:image_url,
+        imageWidth:image_width,
+        imageHeight:image_height,
+        gpuTimeHours:gpu_time_hours,
+        totalGenerations:total_generations,
+        aiOption:ai_option (
+          name,
+          url
+        ),
+        owner:public_user (
+          name,
+          url
+        )
+  		`
+		);
+	return { data, error };
+}
--- src/routes/collections/+page.server.ts
+++ src/routes/collections/+page.server.ts
@@ -1,35 +1,9 @@
-import { supabase } from '$ts/constants/supabase';
-import type { TDBCollectionShallow } from '$ts/types/db';
-import type { PostgrestError } from '@supabase/supabase-js';
+import { getCollectionsShallow } from '$ts/queries/getCollectionsShallow';
 import type { ServerLoad } from '@sveltejs/kit';
 
 export const load: ServerLoad = async () => {
-	const {
-		data: collections,
-		error
-	}: { data: TDBCollectionShallow[] | null; error: PostgrestError | null } = await supabase
-		.from('art_collection')
-		.select(
-			`
-        id,
-        name,
-        slug,
-        imageUrl:image_url,
-        imageWidth:image_width,
-        imageHeight:image_height,
-        gpuTimeHours:gpu_time_hours,
-        totalGenerations:total_generations,
-        aiOption:ai_option (
-          name,
-          url
-        ),
-        owner:public_user (
-          name,
-          url
-        )
-  		`
-		);
+	const { data, error } = await getCollectionsShallow();
 	return {
-		collections
+		collections: data
 	};
 };
