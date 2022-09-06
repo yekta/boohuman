e4fa313615f06bbbb4c09ac53ceb48f13b3f26eb
yekta <ngyekta@gmail.com>
2022-09-06 08:53:53 +0300

    Inner height

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,15 @@
 	import Navbar from '$components/navbar/Navbar.svelte';
 
 	import '$css/app.css';
+
+	let innerHeight: number | undefined;
 </script>
 
-<div class="w-full bg-c-bg text-c-on-bg min-h-screen flex flex-col">
+<svelte:window bind:innerHeight />
+<div
+	class="w-full bg-c-bg text-c-on-bg min-h-screen flex flex-col"
+	style={innerHeight !== undefined ? `min-height: ${innerHeight}px` : ''}
+>
 	<Navbar />
 	<main class="w-full flex-1 flex flex-col overflow-x-hidden">
 		<slot />
