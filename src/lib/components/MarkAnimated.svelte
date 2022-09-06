badcc03e1e0a2bc35a9c7995137fa5f08e84f651
yekta <40706601+yekta@users.noreply.github.com>
2022-09-06 12:05:02 +0300

    Logo change

--- src/lib/components/MarkAnimated.svelte
+++ src/lib/components/MarkAnimated.svelte
@@ -2,11 +2,11 @@
 	class="w-1/3 md:w-40 max-w-full aspect-square flex flex-col relative origin-bottom move-animation overflow-hidden"
 >
 	<div class="w-full h-full p-gradient-135 absolute" />
-	<div class="w-full flex mt-[35.714286%] h-[28.571429%] justify-center relative">
-		<div class="w-[14.285714%] h-full bg-c-bg mr-[14.285714%] origin-bottom blink-animation" />
-		<div class="w-[14.285714%] h-full bg-c-bg origin-bottom blink-animation" />
+	<div class="w-full flex mt-[40.625%] h-[28.125%] justify-center relative">
+		<div class="w-[12.5%] h-full bg-c-bg mr-[12.5%] origin-bottom blink-animation" />
+		<div class="w-[12.5%] h-full bg-c-bg origin-bottom blink-animation" />
 	</div>
-	<div class="w-[150%] -ml-[25%] bg-c-bg h-[7.142857%] mt-[14.285714%] relative mouth-animation" />
+	<div class="w-[150%] -ml-[25%] bg-c-bg h-[6.25%] mt-[12.5%] relative mouth-animation" />
 </div>
 
 <style>
