7815851c4a177e0d0be0b2cc04338204a946f455
yekta <40706601+yekta@users.noreply.github.com>
2022-09-15 01:19:42 +0300

    Encoded images

--- src/lib/ts/constants/imgproxy.ts
+++ src/lib/ts/constants/imgproxy.ts
@@ -1,13 +1,29 @@
-const baseUrl = 'https://imgproxy.boohuman.com';
+import { Buffer } from 'buffer';
 
-export function srcFromUrl(url: string) {
-	return `${baseUrl}/insecure/plain/${url}`;
+const imgproxyUrl = 'https://imgproxy.boohuman.com';
+
+export function srcFromUrl(url: string, size: number | undefined = undefined) {
+	const urlSafeBase64 = (string: string | Buffer) => {
+		return Buffer.from(string)
+			.toString('base64')
+			.replace(/=/g, '')
+			.replace(/\+/g, '-')
+			.replace(/\//g, '_');
+	};
+
+	const resizing_type = 'fit';
+	const extension = url.split('.')[url.split('.').length - 1];
+	const encoded_url = urlSafeBase64(url);
+	const path = `/insecure/${
+		size !== undefined ? `rs:${resizing_type}:${size}:${size}/` : ``
+	}${encoded_url}.${extension}`;
+	return imgproxyUrl + path;
 }
 
 export function srcsetFromUrl(url: string, sizes: number[] = [512, 768, 1024, 1536, 2048]) {
 	let srcset = '';
 	sizes.forEach((size) => {
-		srcset += `${baseUrl}/insecure/rs:fit:${size}:0/plain/${url} ${size}w, `;
+		srcset += `${srcFromUrl(url, size)} ${size}w, `;
 	});
 	return srcset;
 }
